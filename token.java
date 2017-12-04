30370c5588fc3bcac04790c568a5f22cbd4429ec
Fco <francisco.olivars.14@sansano.usm.cl>
2017-12-03 21:54:38 -0300

    Borrar prototipo

--- token.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface token extends Remote {
-    void tomar(int id) throws RemoteException;
-    int estado() throws RemoteException;
-    int liberar(List<Integer> RN) throws RemoteException;
-}
